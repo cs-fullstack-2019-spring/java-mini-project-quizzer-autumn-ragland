83a62e4e68c504085dc7113bdcc24cc390db648a
autumn-ragland <raglanda998@gmail.com>
2019-05-21 16:01:48 -0500

    afternoon push

--- src/Main.java
+++ src/Main.java
@@ -3,27 +3,49 @@ import java.util.*;
 
 public class Main {
 
-    private static void getQuestion(){
-        HashMap<String, String> quizSet = new HashMap<>();
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+//        prompt user for name
+        System.out.print("Please Enter Your Name: ");
+        String userName = read.nextLine();
+
+        System.out.println("User: " + userName);
+
+//        create hashmap of questions with position keys
+        HashMap<Integer, String> quizSet = new HashMap<>();
         try{
+//            read file
             File file = new File("quizzer.txt");
-            Scanner read = new Scanner(file);
-            while (read.hasNextLine()){
-//                System.out.println(read.nextLine());
-                String txt = read.nextLine();
-                String question = txt.split(",")[0];
-                String answer = txt.split(",")[5];
-//                System.out.println(question);
-//                System.out.println(answer);
-                quizSet.put(question,answer);
+            Scanner readFile = new Scanner(file);
+            Integer count = 0;
+//            fill hashmap with questions
+            while (readFile.hasNextLine()){
+                String txt = readFile.nextLine();
+                Integer indexPosition = count;
+                String question = txt;
+                quizSet.put(indexPosition,question);
+                count ++;
             }
         }
         catch (Exception e){
             System.out.println(e.getMessage());
         }
-        System.out.println(quizSet);
+//        hashmap for random questions
+        HashMap<Integer, String> randomSet = new HashMap<>();
+//        prompt user with randomized questions
+        for (int i : printRandomNumbers(5,6)) {
+            if(quizSet.get(i) != null){
+                Integer count = 0;
+                System.out.println(quizSet.get(i));
+                randomSet.put(count,quizSet.get(i));
+                String answer = read.nextLine();
+                count ++;
+            }
+        }
+        System.out.println("Thanks " + userName);
     }
 
+//    get some amount of random numbers (n) between some range(maxRange)
     private static final Random gen = new Random();
     private static int[] printRandomNumbers(int n, int maxRange) {
         assert n <= maxRange : "cannot get more unique numbers than the size of the range";
@@ -41,35 +63,5 @@ public class Main {
             used.add(newRandom);
         }
         return result;
-    }
-
-    private static void iterateRandom() {
-
-        for (int i : printRandomNumbers(5,6)) {
-            System.out.println(i);
-        }
-
-    }
-
-    private static void promptUser(){
-        Scanner read = new Scanner(System.in);
-
-        System.out.print("Please Enter Your Name: ");
-        String userName = read.nextLine();
-
-        iterateRandom();
-
-        System.out.println("Enter option A, B, C or D");
-        String choice1 = read.next();
-        System.out.println("Enter option A, B, C or D");
-        String choice2 = read.next();
-        System.out.println("Enter option A, B, C or D");
-        String choice3 = read.next();
-
-        System.out.println(userName + " chose " + choice1 + choice2 + choice3);
-    }
-
-    public static void main(String[] args) {
-        getQuestion();
     }
 }
